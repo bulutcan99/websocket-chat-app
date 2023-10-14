7e5a9e448f658f87029b403d74573952347dc6fe
bcgocer <bcgocer@yukatech.com>
2023-10-14 16:45:47 +0300

    feat: add auth route

---
+++ app/api/route/auth.go
@@ -0,0 +1,14 @@
+package route
+
+import (
+	"github.com/bulutcan99/go-websocket/app/api/controller"
+	"github.com/bulutcan99/go-websocket/pkg/middleware"
+	"github.com/gofiber/fiber/v2"
+)
+
+// AuthRoutes Base Route = /v1/auth
+func AuthRoutes(r fiber.Router) {
+	route := r.Group("/v1", middleware.GetNextMiddleWare)
+	route.Post("/register", controller.UserSignUp)
+	route.Post("/login", controller.UserSignIn)
+}
