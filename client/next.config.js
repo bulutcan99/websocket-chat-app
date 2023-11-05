5214e27d9c3fa6342af7fa1d5feb57c82408dd6b
Mehmet Bulam <mehmet3bulam@gmail.com>
2023-11-05 20:31:41 +0300

    feat: dockerfile updated.

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  },
 };
 
 module.exports = nextConfig;
